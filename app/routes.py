26db0d29b6955beceee54c39cb064e1be0ba5a47
gatotui <gatotui14@gmail.com>
2025-01-04 23:03:43 -0300

    feat: listar el menu de la semana

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from . import db
-from .models import Plato, Ensalada, Combinacion, Carbohidrato
+from .models import Plato, Combinacion, Carbohidrato
 from datetime import datetime, date
 from werkzeug.security import generate_password_hash, check_password_hash
 from colorama import Fore, Style
@@ -20,16 +20,35 @@ def printn(message):
     last_color = new_color
     print(f"{new_color}[{info.lineno - 1}] {message}{Style.RESET_ALL}")
 
-def process_datetime(client_timezone, datetime_obj):
-    client_tz = pytz.timezone(client_timezone)
-    client_datetime_tz = client_tz.localize(datetime_obj)
-    client_datetime_utc = client_datetime_tz.astimezone(pytz.utc)   
-    client_datetime_local = client_datetime_utc.astimezone(client_tz)
-    date_local=client_datetime_local.isoformat()
-    utc_iso_format = client_datetime_utc.isoformat()
-    return date_local, utc_iso_format
+# def process_datetime(client_timezone, datetime_obj):
+#     client_tz = pytz.timezone(client_timezone)
+#     client_datetime_tz = client_tz.localize(datetime_obj)
+#     client_datetime_utc = client_datetime_tz.astimezone(pytz.utc)   
+#     client_datetime_local = client_datetime_utc.astimezone(client_tz)
+#     date_local=client_datetime_local.isoformat()
+#     utc_iso_format = client_datetime_utc.isoformat()
+#     return date_local, utc_iso_format
 
 def register_routes(app):
+    @app.route('/week')
+    def week():
+        combinaciones_obj = Combinacion.query.order_by(Combinacion.id).all()
+        
+        dias_data = []
+        for combinacion in combinaciones_obj:
+            dia_info = {
+                "dia": combinacion.dia,
+                "plato_nombre": combinacion.platos.nombre if combinacion.platos else None,
+                "ensalada_nombre": combinacion.ensaladas.nombre if combinacion.ensaladas else None,
+                "plato_imagen": combinacion.platos.imagen if combinacion.platos else None,
+                "plato_ingredientes": combinacion.platos.ingredientes if combinacion.platos else None,
+                "plato_preparacion": combinacion.platos.preparacion if combinacion.platos else None,
+            }
+            dias_data.append(dia_info)
+        
+        return render_template("week.html", dias_data=dias_data)
+
+    
     @app.route('/')
     def index():
         dias_esp = {
